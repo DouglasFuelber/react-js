5e8a35ace21847810db4db9b9dc74c1ca299c02f
DouglasFuelber <dffuelber@gmail.com>
2019-11-08 12:46:06 -0300

    List pagination

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -6,13 +6,36 @@ import './styles.css';
 export default () => {
     useEffect(() => {
         loadProducts();
-    });
+    }, []);
 
     const [products, setProducts] = useState([]);
+    const [productInfo, setProductInfo] = useState({});
+    const [page, setPage] = useState(1);
 
-    const loadProducts = async () => {
-        const response = await api.get('/products');
-        setProducts(response.data.docs);
+    const loadProducts = async (pageNumber = 1) => {
+        const response = await api.get(`/products?page=${pageNumber}`);
+
+        const { docs, ...prodInfo } = response.data;
+
+        setProducts(docs);
+        setProductInfo(prodInfo);
+        setPage(pageNumber);
+    }
+
+    const prevPage = () => {
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+
+        loadProducts(pageNumber);
+    }
+
+    const nextPage = () => {
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        loadProducts(pageNumber);
     }
 
     return <div className="product-list">
@@ -23,5 +46,9 @@ export default () => {
                 <a href="">Acessar</a>
             </article>
         ))}
+        <div className="actions">
+            <button disabled={page === 1} onClick={prevPage}>Anterior</button>
+            <button disabled={page === productInfo.pages} onClick={nextPage}>Próxima</button>
+        </div>
     </div>
 }
