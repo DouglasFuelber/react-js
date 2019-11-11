e328f77a3e18ba0c2776e978cc090fb2ea3db092
DouglasFuelber <dffuelber@gmail.com>
2019-11-11 12:52:42 -0300

    Product detail page

--- huntweb/src/pages/product/index.js
+++ huntweb/src/pages/product/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
-export default  () => {
-    return <h1>Product</h1>;
+import './styles.css';
+
+export default ({ match: { params: { id } } }) => {
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const response = await api.get(`/products/${id}`);
+            setProduct(response.data);
+        }
+        fetchProduct();
+    }, []);
+
+    return (
+        <div className="product-info">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+
+            <p>URL: <a href={product.url} target="_blank" rel="noopener noreferrer">{product.url}</a></p>
+
+        </div>
+    );
 }
